3d84278db7498e8fca497a494bae314359eaea1e
Hafiq <hafiqiqmal93@gmail.com>
2020-10-01 13:57:42 +0800

    Change Guzzle to Zttp. Fix Unit Test. Add setter for http headers. Remove redundant assign. Fix error response. Update Composer.json. Update readme

--- helper/helpers.php
+++ helper/helpers.php
@@ -9,17 +9,3 @@ if (! function_exists('fb_feed')) {
         return new FbFeed();
     }
 }
-
-if (! function_exists('isAppsApproved')) {
-    
-    function isAppsApproved($response) 
-    {
-        // check if got 500 caused by fb Apps dont have approval
-        $fbErrorMessage = 'must be reviewed and approved by Facebook';
-        if ($response['error'] == 500 && isset($response['message']->message, $fbErrorMessage) && strpos($response['message']->message, $fbErrorMessage) !== false) {
-            return false;
-        }
-
-        return true;
-    }
-}
